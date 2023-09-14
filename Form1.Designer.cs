1dd2f4d0ad7015784cfbe639b787eb65589c1005
longhxrndd <dianazelda@hotmail.com>
2023-09-14 10:12:09 -0600

    Logica y diseño

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            btnLoadImage = new System.Windows.Forms.Button();
+            btnReadText = new System.Windows.Forms.Button();
+            txtConsole = new System.Windows.Forms.TextBox();
+
+            txtConsole.Location = new System.Drawing.Point(12, 405);
+            txtConsole.Multiline = true;
+            txtConsole.Name = "txtConsole";
+            txtConsole.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            txtConsole.Size = new System.Drawing.Size(776, 100);
+            txtConsole.BackColor = System.Drawing.Color.Black; // Fondo negro
+            txtConsole.ForeColor = System.Drawing.Color.White; // Letras verdes
+            txtConsole.ReadOnly = true; // Hacerlo de solo lectura
+
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            pictureBox1.Location = new System.Drawing.Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(776, 358);
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            pictureBox1.AllowDrop = true;
+            pictureBox1.DragEnter += new System.Windows.Forms.DragEventHandler(pictureBox1_DragEnter);
+            pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(pictureBox1_DragDrop);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+
+            // 
+            // btnLoadImage
+            // 
+            btnLoadImage.Location = new System.Drawing.Point(12, 376);
+            btnLoadImage.Name = "btnLoadImage";
+            btnLoadImage.Size = new System.Drawing.Size(112, 23);
+            btnLoadImage.TabIndex = 1;
+            btnLoadImage.Text = "Cargar Imagen";
+            btnLoadImage.UseVisualStyleBackColor = true;
+            btnLoadImage.Click += new System.EventHandler(btnLoadImage_Click);
+
+            // 
+            // btnReadText
+            // 
+            btnReadText.Location = new System.Drawing.Point(676, 376);
+            btnReadText.Name = "btnReadText";
+            btnReadText.Size = new System.Drawing.Size(112, 23);
+            btnReadText.TabIndex = 2;
+            btnReadText.Text = "Leer Texto";
+            btnReadText.UseVisualStyleBackColor = true;
+            btnReadText.Click += new System.EventHandler(btnReadText_Click);
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 550);
+            Controls.Add(btnReadText);
+            Controls.Add(btnLoadImage);
+            Controls.Add(pictureBox1);
+            Controls.Add(txtConsole);
+            Name = "Form1";
+            Text = "Lector de Imagenes";
+            Load += new System.EventHandler(Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TextBox txtConsole;
+        private PictureBox pictureBox1;
+        private Button btnLoadImage;
+        private Button btnReadText;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,7 @@
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Tesseract;
 namespace Lector
 {
     public partial class Form1 : Form
@@ -6,5 +10,96 @@ namespace Lector
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+
+        private void pictureBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files != null && files.Length != 0)
+            {
+                pictureBox1.Image = Image.FromFile(files[0]);
+            }
+        }
+
+        private void btnReadText_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                // Cargando la imagen en Emgu.CV
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+                UMat uImage = bmp.ToImage<Bgr, Byte>().ToUMat();
+
+                // Convirtiendo la imagen a escala de grises
+                Image<Gray, byte> gray = uImage.ToImage<Gray, byte>();
+
+                // Aplicando desenfoque Gaussiano para resaltar el texto
+                Image<Gray, byte> blurred = gray.SmoothGaussian(5);
+
+                // Creando una nueva instancia de TesseractEngine (añade la ruta correcta a tesseract.exe)
+                using (var ocr = new TesseractEngine(@".\tessdata", "spa", EngineMode.TesseractAndLstm))
+                {
+                    // Realizando OCR en la imagen procesada
+                    using (var pix = Pix.LoadFromMemory(blurred.ToBitmap().ToByteArray(System.Drawing.Imaging.ImageFormat.Png)))
+                    using (var page = ocr.Process(pix))
+                    {
+                        string texto = page.GetText();
+
+                        WriteToConsole(texto);  // Usa esto en lugar de MessageBox.Show(texto);
+                                                // ...
+
+                    }
+
+
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Por favor, carga una imagen primero.");
+            }
+        }
+
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Imagenes|*.jpg;*.png;*.bmp";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = Image.FromFile(ofd.FileName);
+                }
+            }
+        }
+        private void WriteToConsole(string message)
+        {
+            txtConsole.Text = ""; // Limpia el contenido anterior
+            txtConsole.AppendText(message + Environment.NewLine);
+            txtConsole.SelectionStart = txtConsole.Text.Length;
+            txtConsole.ScrollToCaret();
+        }
     }
+
+    public static class ImageExtensions
+    {
+        public static byte[] ToByteArray(this Image image, System.Drawing.Imaging.ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, format);
+                return ms.ToArray();
+            }
+        }
+    }
+
 }
